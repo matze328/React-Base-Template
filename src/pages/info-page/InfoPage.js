9c175b22b1516678976ba0363b594befec7a674c
matze328 <152269645+matze328@users.noreply.github.com>
2024-02-08 20:02:20 +0100

    info page mit  Loading Div init

--- src/pages/info-page/InfoPage.js
+++ src/pages/info-page/InfoPage.js
@@ -1,7 +1,18 @@
 import styles from "./InfoPage.module.css";
+import { useEffect, useState } from "react";
+import LoadingDiv from "../../components/common/templates/loading-div";
+import Checkbox from "../../components/common/buttons/checkbox";
 
 function InfoPage() {
-  return <div className={styles.mainContainer}> This is my InfoPage</div>;
-}
-
+  const [stayLoggedIn, setStayLoggedIn] = useState(false);
+  function onClickStayLoggedIn() {
+    setStayLoggedIn(!stayLoggedIn);
+  }
+       return <div className={styles.mainContainer}> 
+        <div>This is my InfoPage</div>;
+         <LoadingDiv />
+         <Checkbox isChecked={stayLoggedIn} onClick={onClickStayLoggedIn} />
+     </div>
+     }
+    
 export default InfoPage;
